f22016960af9c908eaa7f18aab8dc2d4a79d363c
Deepak282 <79189063+Deepak282@users.noreply.github.com>
2023-09-09 18:27:38 +0530

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { HeadersComponent } from './headers/headers.component';
+import { KeyChainComponent } from './key-chain/key-chain.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    component: HomeComponent,
+    path: ''
+  },
+  {
+    component: KeyChainComponent,
+    path: 'keyChains'
+  }
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HomeComponent } from './home/home.component';
+import { HeadersComponent } from './headers/headers.component';
+import { KeyChainComponent } from './key-chain/key-chain.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeadersComponent,
+    KeyChainComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
